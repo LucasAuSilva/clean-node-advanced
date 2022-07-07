e2abfea309875b7cd98e86c7862966b12c42139d
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-07-07 01:21:45 -0300

    feat(FacebookAuthentication): ensure returns AuthError if LoadFacebookUserApi returns undefined

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,3 +1,4 @@
+import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthenticationDto } from '@/domain/features/facebook-authentication'
 
 class FacebookAuthenticationService {
@@ -5,20 +6,25 @@ class FacebookAuthenticationService {
     private readonly loadFacebookUserByTokenApi: LoadFacebookUserApi
   ) {}
 
-  async perform (dto: FacebookAuthenticationDto): Promise<void> {
+  async perform (dto: FacebookAuthenticationDto): Promise<AuthenticationError> {
     await this.loadFacebookUserByTokenApi.loadUser(dto.token)
+    return new AuthenticationError()
   }
 }
 
 interface LoadFacebookUserApi {
-  loadUser: (token: string) => Promise<void>
+  loadUser: (token: string) => Promise<LoadFacebookUserApiReturn>
 }
+
+type LoadFacebookUserApiReturn = undefined
 
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
   token?: string
+  result = undefined
 
-  async loadUser (token: string): Promise<void> {
+  async loadUser (token: string): Promise<LoadFacebookUserApiReturn> {
     this.token = token
+    return this.result
   }
 }
 
@@ -28,5 +34,13 @@ describe('Facebook Authentication Service', () => {
     const sut = new FacebookAuthenticationService(loadFacebookUserApi)
     await sut.perform({ token: 'any_token' })
     expect(loadFacebookUserApi.token).toBe('any_token')
+  })
+
+  it('should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
+    const loadFacebookUserApi = new LoadFacebookUserApiSpy()
+    loadFacebookUserApi.result = undefined
+    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
+    const authResult = await sut.perform({ token: 'any_token' })
+    expect(authResult).toEqual(new AuthenticationError())
   })
 })
