06953ef226b3873574791cce85ff75ae88fab9f9
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-07-07 22:10:20 -0300

    refactor(AxiosHttpClient): move sut creation to factory method

--- tests/infra/http/client-axios.spec.ts
+++ tests/infra/http/client-axios.spec.ts
@@ -2,31 +2,38 @@ import { HttpGetClientDto } from './client'
 
 import axios from 'axios'
 
+jest.mock('axios')
+
 class AxiosHttpClient {
   async get (dto: HttpGetClientDto): Promise<void> {
     await axios.get(dto.url, { params: dto.params })
   }
 }
 
-jest.mock('axios')
+type SutTypes = {
+  sut: AxiosHttpClient
+  fakeAxios: jest.Mocked<typeof axios>
+}
+
+const makeSut = (): SutTypes => {
+  const fakeAxios = axios as jest.Mocked<typeof axios>
+  const sut = new AxiosHttpClient()
+  return {
+    sut,
+    fakeAxios
+  }
+}
+
+const url = 'any_url'
+const params = { any: 'any' }
 
 describe('AxiosHttp Client', () => {
   describe('get', () => {
-    it('should call get with correct params', async () => {
-      const fakeAxios = axios as jest.Mocked<typeof axios>
-      const sut = new AxiosHttpClient()
-      await sut.get({
-        url: 'any_url',
-        params: {
-          any: 'any'
-        }
-      })
+    it('should call get with correct values', async () => {
+      const { sut, fakeAxios } = makeSut()
+      await sut.get({ url, params })
 
-      expect(fakeAxios.get).toHaveBeenCalledWith('any_url', {
-        params: {
-          any: 'any'
-        }
-      })
+      expect(fakeAxios.get).toHaveBeenCalledWith(url, { params })
       expect(axios.get).toHaveBeenCalledTimes(1)
     })
   })
