16b3bedfc71fdd96fba5614f2705d3130d29a87a
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-07-09 14:01:47 -0300

    refactor(FacebookLoginController): use generics instead of any

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -3,12 +3,18 @@ import { FacebookAuth } from '@/domain/features/facebook-authentication'
 import { badRequest, unauthorized, HttpResponse, ok, serverError } from '@/application/helpers'
 import { RequiredFieldError } from '@/application/errors'
 
+type HttpRequest = {
+  token: string | undefined | null
+}
+
+type Model = Error | { accessToken: string }
+
 export class FacebookLoginController {
   constructor (
     private readonly facebookAuth: FacebookAuth
   ) {}
 
-  async handle (httpRequest: any): Promise<HttpResponse> {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
     try {
       if (httpRequest.token === '' || httpRequest.token === null || httpRequest.token === undefined) {
         return badRequest(new RequiredFieldError('token'))
--- src/application/helpers/http.ts
+++ src/application/helpers/http.ts
@@ -1,26 +1,26 @@
 import { ServerError, UnauthorizedError } from '@/application/errors'
 
-export type HttpResponse = {
+export type HttpResponse<T = any> = {
   statusCode: number
-  data: any
+  data: T
 }
 
-export const ok = (result: any): HttpResponse => ({
+export const ok = <T = any> (result: T): HttpResponse<T> => ({
   statusCode: 200,
   data: result
 })
 
-export const badRequest = (error: Error): HttpResponse => ({
+export const badRequest = (error: Error): HttpResponse<Error> => ({
   statusCode: 400,
   data: error
 })
 
-export const unauthorized = (): HttpResponse => ({
+export const unauthorized = (): HttpResponse<Error> => ({
   statusCode: 401,
   data: new UnauthorizedError()
 })
 
-export const serverError = (error: Error): HttpResponse => ({
+export const serverError = (error: Error): HttpResponse<Error> => ({
   statusCode: 500,
   data: new ServerError(error)
 })
