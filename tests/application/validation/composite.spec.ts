24e439eacb0a1bc7dcb5b0a9b222fde04a80a529
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-07-09 15:56:58 -0300

    refactor(ValidationComposite): isolate SUT creation

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -12,14 +12,27 @@ class ValidationComposite {
   }
 }
 
+type SutTypes = {
+  sut: ValidationComposite
+  validators: Validator[]
+}
+
+const makeSut = (): SutTypes => {
+  const firstValidator = mock<Validator>()
+  firstValidator.validate.mockReturnValue(undefined)
+  const secondValidator = mock<Validator>()
+  secondValidator.validate.mockReturnValue(undefined)
+  const validators = [firstValidator, secondValidator]
+  const sut = new ValidationComposite(validators)
+  return {
+    sut,
+    validators
+  }
+}
+
 describe('Validation Composite', () => {
   it('should return undefined if all Validators returns undefined', () => {
-    const firstValidator = mock<Validator>()
-    firstValidator.validate.mockReturnValue(undefined)
-    const secondValidator = mock<Validator>()
-    secondValidator.validate.mockReturnValue(undefined)
-    const validators = [firstValidator, secondValidator]
-    const sut = new ValidationComposite(validators)
+    const { sut } = makeSut()
 
     const error = sut.validate()
 
