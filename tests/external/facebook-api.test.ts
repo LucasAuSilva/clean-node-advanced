e0925aa6cdd47ade697d8936e4907f6b58bf4465
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-07-13 09:25:52 -0300

    refactor(FacebookApi): isolate SUT creation

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -2,14 +2,27 @@ import { FacebookApi } from '@/infra/apis/facebook'
 import { AxiosHttpClient } from '@/infra/http'
 import { env } from '@/main/config/env'
 
+type SutTypes = {
+  sut: FacebookApi
+  axiosClient: AxiosHttpClient
+}
+
+const makeSut = (): SutTypes => {
+  const axiosClient = new AxiosHttpClient()
+  const sut = new FacebookApi(
+    axiosClient,
+    env.facebookApi.clientId,
+    env.facebookApi.clientSecret
+  )
+  return {
+    sut,
+    axiosClient
+  }
+}
+
 describe('Facebook API Integration Tests', () => {
   it('should return a facebook user if token is valid', async () => {
-    const axiosClient = new AxiosHttpClient()
-    const sut = new FacebookApi(
-      axiosClient,
-      env.facebookApi.clientId,
-      env.facebookApi.clientSecret
-    )
+    const { sut } = makeSut()
 
     // Access token only valid for 3 months, check before test fails
     const fbUser = await sut.loadUser({ token: 'EABLuhIey9AsBAF0UkCeHZAjmV4Gs4PtYJcJxrevfPtlW71Qea5jvhQ4IlXSVxRUGwfiMGZCRRhDCDaIFXzLq4iqdsVVC2nZAlM1dYjdeoOMr1yZBYhT0nkeqQAI0qNncZB6l5vTBYkomtgd3dK3TOdKjA11hgZBPvtUDQoAAdW6jMIHHvSSXaoOdvCbDqORQWpYZA5ZAFKCRyAZDZD' })
@@ -22,12 +35,7 @@ describe('Facebook API Integration Tests', () => {
   })
 
   it('should return undefined if token is invalid', async () => {
-    const axiosClient = new AxiosHttpClient()
-    const sut = new FacebookApi(
-      axiosClient,
-      env.facebookApi.clientId,
-      env.facebookApi.clientSecret
-    )
+    const { sut } = makeSut()
 
     // Access token only valid for 3 months, check before test fails
     const fbUser = await sut.loadUser({ token: 'invalid' })
