c7960abdce14e9cab2d12b88db87b50c4f4d8f13
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-07-15 13:13:29 -0300

    feat(AuthenticationMiddleware): ensure return 403 if authorization is empty

--- src/application/errors/http.ts
+++ src/application/errors/http.ts
@@ -20,3 +20,10 @@ export class UnauthorizedError extends Error {
     this.name = 'UnauthorizedError'
   }
 }
+
+export class ForbiddenError extends Error {
+  constructor () {
+    super('Access Denied')
+    this.name = 'ForbiddenError'
+  }
+}
---
+++ tests/application/middlewares/authentication.spec.ts
@@ -0,0 +1,45 @@
+import { HttpResponse, forbidden } from '@/application/helpers'
+import { ForbiddenError } from '@/application/errors'
+
+type HttpRequest = { authorization: string }
+
+class AuthenticationMiddleware {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse<Error>> {
+    return forbidden()
+  }
+}
+
+describe('Authentication Middleware', () => {
+  it('should return 403 if authorization is empty', async () => {
+    const sut = new AuthenticationMiddleware()
+
+    const httpResponse = await sut.handle({ authorization: '' })
+
+    expect(httpResponse).toEqual({
+      statusCode: 403,
+      data: new ForbiddenError()
+    })
+  })
+
+  it('should return 403 if authorization is null', async () => {
+    const sut = new AuthenticationMiddleware()
+
+    const httpResponse = await sut.handle({ authorization: null as any })
+
+    expect(httpResponse).toEqual({
+      statusCode: 403,
+      data: new ForbiddenError()
+    })
+  })
+
+  it('should return 403 if authorization is undefined', async () => {
+    const sut = new AuthenticationMiddleware()
+
+    const httpResponse = await sut.handle({ authorization: undefined as any })
+
+    expect(httpResponse).toEqual({
+      statusCode: 403,
+      data: new ForbiddenError()
+    })
+  })
+})
