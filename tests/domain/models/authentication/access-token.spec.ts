148c9a72ad1dd89458d794db86994b82de7faffc
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-07-15 10:48:26 -0300

    refactor: change access token to object

--- tests/domain/models/authentication/access-token.spec.ts
+++ tests/domain/models/authentication/access-token.spec.ts
@@ -1,12 +1,6 @@
 import { AccessToken } from '@/domain/models/authentication'
 
 describe('Access token', () => {
-  it('should create with a value', () => {
-    const sut = new AccessToken('any_value')
-
-    expect(sut).toEqual({ value: 'any_value' })
-  })
-
   it('should expire in 180_00_00 ms', () => {
     expect(AccessToken.expirationInMs).toBe(180_00_00)
   })
