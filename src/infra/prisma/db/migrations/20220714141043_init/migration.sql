d014f31b78a5a28ce1df7664a72b62de8b0d83ec
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-07-18 14:21:21 -0300

    chore: change postgres to sqlite to test

--- src/infra/prisma/db/migrations/20220714141043_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "usuarios" (
-    "id" SERIAL NOT NULL,
-    "nome" TEXT,
-    "email" TEXT NOT NULL,
-    "id_facebook" TEXT,
-
-    CONSTRAINT "usuarios_pkey" PRIMARY KEY ("id")
-);
