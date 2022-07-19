6cf66f90acbda184c8020642b938fc65c4bb28db
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-07-19 08:30:57 -0300

    feat(ChangeProfilePicture): ensure calls DeleteFile if file exists and SaveProfilePicture throws

--- src/data/services/change-profile-picture.ts
+++ src/data/services/change-profile-picture.ts
@@ -1,27 +1,31 @@
 import { Profile } from '@/domain/models/profile'
-import { UploadFile } from '@/data/contracts/file-storage'
+import { DeleteFile, UploadFile } from '@/data/contracts/file-storage'
 import { UUIDGenerator } from '@/data/contracts/crypto'
 import { LoadProfileById, SaveProfilePicture } from '@/data/contracts/repositories/profile'
 
 export class ChangeProfilePicture {
   constructor (
-    private readonly fileStorage: UploadFile,
+    private readonly fileStorage: UploadFile & DeleteFile,
     private readonly uniqueId: UUIDGenerator,
     private readonly profileRepo: SaveProfilePicture & LoadProfileById
   ) {}
 
   async perform ({ id, file }: ChangeProfilePictureDto): Promise<ChangeProfilePictureResult> {
+    const uuid = this.uniqueId.generate(id)
     let pictureUrl: string | undefined
     let name: string | undefined
     if (file !== undefined) {
-      const uuid = this.uniqueId.generate(id)
       pictureUrl = await this.fileStorage.upload(file, uuid)
     } else {
       name = await this.profileRepo.loadById(id)
     }
     const profile = new Profile(id)
     profile.setPicture(pictureUrl, name)
-    await this.profileRepo.savePicture(profile.id, profile.pictureUrl, profile.initials)
+    try {
+      await this.profileRepo.savePicture(profile.id, profile.pictureUrl, profile.initials)
+    } catch {
+      await this.fileStorage.delete(uuid)
+    }
     return profile
   }
 }
--- tests/data/services/change-profile-picture.spec.ts
+++ tests/data/services/change-profile-picture.spec.ts
@@ -1,6 +1,6 @@
 import { Profile } from '@/domain/models/profile'
 import { ChangeProfilePicture } from '@/data/services'
-import { UploadFile } from '@/data/contracts/file-storage'
+import { DeleteFile, UploadFile } from '@/data/contracts/file-storage'
 import { UUIDGenerator } from '@/data/contracts/crypto'
 import { LoadProfileById, SaveProfilePicture } from '@/data/contracts/repositories/profile'
 
@@ -10,7 +10,7 @@ jest.mock('@/domain/models/profile/user')
 
 type SutTypes = {
   sut: ChangeProfilePicture
-  fileStorage: MockProxy<UploadFile>
+  fileStorage: MockProxy<UploadFile & DeleteFile>
   uniqueId: MockProxy<UUIDGenerator>
   profileRepo: MockProxy<SaveProfilePicture & LoadProfileById>
 }
@@ -20,7 +20,7 @@ const makeSut = (): SutTypes => {
   profileRepo.loadById.mockResolvedValue('Lucas Augusto da Silva')
   const uniqueId = mock<UUIDGenerator>()
   uniqueId.generate.mockReturnValue('any_unique_id')
-  const fileStorage = mock<UploadFile>()
+  const fileStorage = mock<UploadFile & DeleteFile>()
   fileStorage.upload.mockResolvedValue('any_url_image')
   const sut = new ChangeProfilePicture(fileStorage, uniqueId, profileRepo)
   return {
@@ -58,7 +58,7 @@ describe('Change Profile Picture', () => {
   })
 
   it('should return correct data on success', async () => {
-    jest.mocked(Profile).mockImplementationOnce(id => ({
+    jest.mocked(Profile).mockImplementationOnce(_id => ({
       setPicture: jest.fn(),
       id: 'any_id',
       pictureUrl: 'any_url',
@@ -83,5 +83,15 @@ describe('Change Profile Picture', () => {
     await sut.perform({ id, file: undefined })
     expect(profileRepo.loadById).toHaveBeenCalledWith(id)
     expect(profileRepo.loadById).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call DeleteFile when file exists and SaveProfilePicture throws', async () => {
+    const { sut, profileRepo, fileStorage } = makeSut()
+    profileRepo.savePicture.mockRejectedValueOnce(new Error())
+    const promise = sut.perform({ id, file })
+    promise.catch(() => {
+      expect(fileStorage.delete).toHaveBeenCalledWith({ key: uuid })
+      expect(fileStorage.delete).toHaveBeenCalledTimes(1)
+    })
   })
 })
