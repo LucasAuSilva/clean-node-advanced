44bed6d4a196a4bfb297439685ce228a5c0d467a
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-07-19 18:39:04 -0300

    feat(UUIDHandler): ensure returns correct value

--- tests/infra/crypto/uuid-handler.spec.ts
+++ tests/infra/crypto/uuid-handler.spec.ts
@@ -3,17 +3,24 @@ import { v4 } from 'uuid'
 jest.mock('uuid')
 
 class UUIDHandler {
-  generate (key: string): void {
-    v4()
+  generate (key: string): string {
+    return `${key}_${v4()}`
   }
 }
 
 describe('UUID Handler', () => {
   it('should call uuid.v4', () => {
     const sut = new UUIDHandler()
-
     sut.generate('any_key')
-
     expect(v4).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return correct uuid', () => {
+    jest.mocked(v4).mockReturnValueOnce('any_uuid')
+    const sut = new UUIDHandler()
+
+    const uuid = sut.generate('any_key')
+
+    expect(uuid).toBe('any_key_any_uuid')
   })
 })
