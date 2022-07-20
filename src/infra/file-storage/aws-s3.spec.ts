112f52758326c70cc654e3a766bfad3ae93ad781
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-07-19 22:04:36 -0300

    feat(AwsS3FileStorage): ensure calls putObject with correct input

--- src/infra/file-storage/aws-s3.spec.ts
+++ src/infra/file-storage/aws-s3.spec.ts
@@ -1,9 +1,12 @@
-import { config } from 'aws-sdk'
+import { config, S3 } from 'aws-sdk'
 
 jest.mock('aws-sdk')
 
 class AwsS3FileStorage {
-  constructor (private readonly accessKey: string, private readonly secret: string) {
+  constructor (
+    accessKey: string, secret: string,
+    private readonly bucket: string
+  ) {
     config.update({
       credentials: {
         accessKeyId: accessKey,
@@ -11,16 +14,40 @@ class AwsS3FileStorage {
       }
     })
   }
+
+  async upload (key: string, file: Buffer): Promise<void> {
+    const s3 = new S3()
+    await s3.putObject({
+      Bucket: this.bucket,
+      Key: key,
+      Body: file,
+      ACL: 'public-read'
+    }).promise()
+  }
 }
 
 const accessKey = 'any_access_key'
 const secret = 'any_secret'
+const bucket = 'any_bucket'
+const key = 'any_key'
+const file = Buffer.from('any_buffer')
 
 const makeSut = (): AwsS3FileStorage => {
-  return new AwsS3FileStorage(accessKey, secret)
+  return new AwsS3FileStorage(accessKey, secret, bucket)
 }
 
 describe('AWS S3 File Storage', () => {
+  let putObjectSpy: jest.Mock
+  let putObjectPromiseSpy: jest.Mock
+
+  beforeAll(() => {
+    putObjectPromiseSpy = jest.fn()
+    putObjectSpy = jest.fn().mockImplementation(() => ({ promise: putObjectPromiseSpy }))
+    jest.mocked(S3).mockImplementation(jest.fn().mockImplementation(() => ({
+      putObject: putObjectSpy
+    })))
+  })
+
   it('should config aws credentials on creation', () => {
     const sut = makeSut()
 
@@ -32,5 +59,18 @@ describe('AWS S3 File Storage', () => {
       }
     })
     expect(config.update).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call put object with correct input', async () => {
+    const sut = makeSut()
+    await sut.upload(key, file)
+    expect(putObjectSpy).toHaveBeenCalledWith({
+      Bucket: bucket,
+      Key: key,
+      Body: file,
+      ACL: 'public-read'
+    })
+    expect(putObjectSpy).toHaveBeenCalledTimes(1)
+    expect(putObjectPromiseSpy).toHaveBeenCalledTimes(1)
   })
 })
