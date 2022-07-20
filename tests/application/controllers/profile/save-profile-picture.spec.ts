35c2f3e525904a08e2e43a159bd6c86401726cdf
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-07-20 08:57:03 -0300

    refactor(SavePictureController): isolate SUT creation

--- tests/application/controllers/profile/save-profile-picture.spec.ts
+++ tests/application/controllers/profile/save-profile-picture.spec.ts
@@ -10,11 +10,31 @@ class SavePictureController {
   }
 }
 
+type SutTypes = {
+  sut: SavePictureController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new SavePictureController()
+  return {
+    sut
+  }
+}
+
 describe('SavePicture Controller', () => {
   it('should return 400 if file is not provided', async () => {
-    const sut = new SavePictureController()
-
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({ file: undefined })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('file')
+    })
+  })
+
+  it('should return 400 if file is not provided', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({ file: null })
 
     expect(httpResponse).toEqual({
       statusCode: 400,
